2d50d397e7ca6671810a34c54f5dcfc3e3dc6a98
Toni Pohl <tp@atwork.at>
2019-02-26 12:52:58 +0100

    intial commit

--- get-subscription-inventory.ps1
+++ get-subscription-inventory.ps1
@@ -1,7 +1,7 @@
 # get-subscription-inventory.ps1
 # Reads all resources in the selected subscription and writes the data to a CSV file.
 # atwork.at, TP, 25.02.2019
-$result = "C:\Temp\PS\result.csv" 
+$result = "C:\Temp\result.csv" 
 
 "SubscriptionName,SubscriptionId,Resource,Name,ResourceGroupName,ResourceId" `
 		| out-file $OutFilePath -encoding ascii
